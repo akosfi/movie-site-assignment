85b77fa3829723668707641318f0d60c3e377310
akosfi <fiakos1997@gmail.com>
2023-10-12 12:31:37 +0200

    [feat] Handle image load failing.

--- src/client/modules/movies/movie-listing-page/components/listing-page/components/listing-item/ListingItem.tsx
+++ src/client/modules/movies/movie-listing-page/components/listing-page/components/listing-item/ListingItem.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import css from './ListingItem.module.scss';
 import { Card } from 'client/modules/data-display/card';
 import { Ellipse } from 'client/modules/data-display/ellipse';
@@ -10,20 +10,34 @@ type ListingItemProps = {
 
 const imageBaseUrl = 'https://image.tmdb.org/t/p/w500';
 
-const ListingItem: FC<ListingItemProps> = ({ movie }) => (
-    <Card className={css['card']}>
-        <div className={css['image']}>
-            <img src={`${imageBaseUrl}/${movie.posterPath}`} alt="item image" />
-        </div>
+const ListingItem: FC<ListingItemProps> = ({ movie }) => {
+    const [imageLoadFailed, setImageLoadFailed] = useState(false);
 
-        <p className={css['name']}>
-            <Ellipse label={movie.title} length={20} />
-        </p>
-        <p className={css['releaseDate']}>{movie.releaseDate}</p>
-        <p className={css['overview']}>
-            <Ellipse label={movie.overview} length={100} />
-        </p>
-    </Card>
-);
+    const handleImageLoadError = () => setImageLoadFailed(true);
+
+    return (
+        <Card className={css['card']}>
+            <div className={css['image']}>
+                {!imageLoadFailed ? (
+                    <img
+                        src={`${imageBaseUrl}/${movie.posterPath}`}
+                        alt="item image"
+                        onError={handleImageLoadError}
+                    />
+                ) : (
+                    <span>Poster cannot be loaded.</span>
+                )}
+            </div>
+
+            <p className={css['name']}>
+                <Ellipse label={movie.title} length={20} />
+            </p>
+            <p className={css['releaseDate']}>{movie.releaseDate}</p>
+            <p className={css['overview']}>
+                <Ellipse label={movie.overview} length={100} />
+            </p>
+        </Card>
+    );
+};
 
 export default ListingItem;
